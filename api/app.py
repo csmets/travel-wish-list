fe563d7279f2fbabe7e205fd3b8124c186bed1c8
Clyde Smets <clyde.smets@gmail.com>
2017-11-05 17:37:08 +1000

    Added cors to api

--- api/app.py
+++ api/app.py
@@ -8,7 +8,9 @@ update users.
 
 import falcon
 
-from countries import Country
+from falcon_cors import CORS
+
+from countries import Countries, Country
 
 from cities import City
 
@@ -20,8 +22,11 @@ from votes import Vote
 
 from login import Login
 
+cors = CORS(allow_origins_list=['http://localhost:8080'])
 
-api = falcon.API()
+api = falcon.API(middleware=[cors.middleware])
+
+api.add_route('/countries', Countries())
 
 api.add_route('/country/{country}', Country())
 
