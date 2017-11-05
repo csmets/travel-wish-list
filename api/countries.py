e9ab5d4f383bfce9d75421b18bbe1e860c828a37
Clyde Smets <clyde.smets@gmail.com>
2017-11-05 17:37:40 +1000

    Added listing of all countries

--- api/countries.py
+++ api/countries.py
@@ -17,3 +17,29 @@ class Country(object):
         resp.content_type = 'application/json'
 
         resp.status = falcon.HTTP_200
+
+class Countries(object):
+
+    def on_get(self, req, resp):
+
+        db = Database()
+
+        countries_data = db.fetchall('countries')
+
+        country_data = []
+
+        for country in countries_data:
+
+            country_record = dict()
+
+            country_record['name'] = country['name']
+
+            country_record['code'] = country['code']
+
+            country_data.append(country_record)
+
+        resp.body = json.dumps(country_data)
+
+        resp.content_type = 'application/json'
+
+        resp.status = falcon.HTTP_200
